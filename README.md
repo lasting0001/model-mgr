79571d735281c97f5a01c947c8c4a66325dc6f6d
li.j <li.j1@yuwan8.com>
2015-11-02 12:09:00 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# cache4js
+A model manager for nodejs API.With it,you can handle routes easily.  
+
+##How to use ?   
+1、install  
+npm install model-mgr   
+2、use
+````javascript
+var models = require('ext4js')(__dirname);
+
+for global:
+
+global.MODELS = require('ext4js')(__dirname);
+````
+For more details,please refer to the code.   
+   
+   
+## Contributing   
+Contributions welcome   
+   
+## License   
+The original cache4js was distributed under the Apache 2.0 License, and so is this. I've tried to   
+keep the original copyright and author credits in place, except in sections that I have rewritten   
+extensively.   
+   
---
+++ src/model_mgr.js
@@ -0,0 +1,42 @@
+/**
+ * Created by jun.li on 11/02.
+ */
+'use strict';
+var _file = require('fs');
+var _path = require('path');
+function ModelsMgr() {
+    return function (dir) {
+        var files = [];
+        var models = {};
+        dir = dir || __dirname;
+        var init_dir_len = dir.length;
+        var getAllFiles = function (dir, parent_dir) {
+            parent_dir = parent_dir || '';
+            var fs = _file.readdirSync(dir), temp_name = '', temp_path = '';
+            fs.forEach(function (e) {
+                if (e.indexOf('auto_routes.js') !== -1 || e.indexOf('index.js') !== -1) {
+                    return;
+                }
+                if (e.indexOf('.') === -1) {
+                    return getAllFiles(_path.join(dir, '/', e), e);
+                }
+                temp_name = e.split('.')[0];
+                temp_path = dir.substr(init_dir_len).replaceAll('\\\\', '/');
+                parent_dir && (temp_name = temp_path + '/' + temp_name);
+                (!parent_dir) && (temp_name = '/');
+                files.push({file: _path.join(dir, '/', e), name: temp_name});
+            });
+        };
+        var loadModules = function () {
+            files.forEach(function (e) {
+                models[e.name] = require(e.file);
+            });
+        };
+        getAllFiles(dir);
+        loadModules();
+
+        return models;
+    };
+}
+
+module.exports = ModelsMgr();
